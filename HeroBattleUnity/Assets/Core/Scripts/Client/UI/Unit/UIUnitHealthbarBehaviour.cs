4a2f30c93d1745dd73a5b26adf8e2d0af944d6c9
pearl2201 <nguyenanhngoc.ftu@gmail.com>
2024-10-16 22:10:46 +0700

    no message

--- HeroBattleUnity/Assets/Core/Scripts/Client/UI/Unit/UIUnitHealthbarBehaviour.cs
+++ HeroBattleUnity/Assets/Core/Scripts/Client/UI/Unit/UIUnitHealthbarBehaviour.cs
@@ -1,45 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-namespace DragonCrashers
-{
-    public class UIUnitHealthbarBehaviour : MonoBehaviour
-    {
-        [Header("Unit Health")]
-        public UnitHealthBehaviour healthBehaviour;
+//namespace DragonCrashers
+//{
+//    public class UIUnitHealthbarBehaviour : MonoBehaviour
+//    {
+//        [Header("Unit Health")]
+//        public UnitHealthBehaviour healthBehaviour;
 
-        [Header("UI References")]
-        public UISliderBehaviour healthSlider;
+//        [Header("UI References")]
+//        public UISliderBehaviour healthSlider;
 
-        void OnEnable()
-        {
-            healthBehaviour.HealthChangedEvent += UpdateHealthDisplay;
-        }
+//        void OnEnable()
+//        {
+//            healthBehaviour.HealthChangedEvent += UpdateHealthDisplay;
+//        }
 
-        void OnDisable()
-        {
-            healthBehaviour.HealthChangedEvent -= UpdateHealthDisplay;
-        }
+//        void OnDisable()
+//        {
+//            healthBehaviour.HealthChangedEvent -= UpdateHealthDisplay;
+//        }
 
-        void Start()
-        {
-            SetupHealthDisplay();
-        }
+//        void Start()
+//        {
+//            SetupHealthDisplay();
+//        }
 
-        void SetupHealthDisplay()
-        {
-            int totalHealth = healthBehaviour.GetCurrentHealth();
-            healthSlider.SetupDisplay((float)totalHealth);
+//        void SetupHealthDisplay()
+//        {
+//            int totalHealth = healthBehaviour.GetCurrentHealth();
+//            healthSlider.SetupDisplay((float)totalHealth);
             
-            UpdateHealthDisplay(totalHealth);
-        }
+//            UpdateHealthDisplay(totalHealth);
+//        }
 
-        void UpdateHealthDisplay(int newHealthAmount)
-        {
-            healthSlider.SetCurrentValue((float)newHealthAmount);
-        }
+//        void UpdateHealthDisplay(int newHealthAmount)
+//        {
+//            healthSlider.SetCurrentValue((float)newHealthAmount);
+//        }
 
-    }
-}
+//    }
+//}
 
